063720e6e7a8be1dd34843aede0edfcb50dc358c
steelcrust <steelcrustobj@163.com>
2018-01-05 15:08:29 +0800

    system

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>  
+  <div class="container-fluid" id="app">
+    <top></top>
+    <right></right>
+    <router-view></router-view>
+  </div>    
+</template>
+
+<script>
+import top from './components/public/top'
+import right from './components/public/right'
+export default {
+  name: 'app',
+  components: {
+    top,
+    right
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/public/top.vue
@@ -0,0 +1,50 @@
+<template>
+    <header class="row header clearfix" style="position:relative;">
+        <div class="pull-left pad0 w315">
+            <ul class="clearfix">
+                <li class="pull-left wb50 text-left"><img src='/lib/img/public/logo.png' class="logo mt5 ml35"/></li>
+                <li class="pull-left wb40">
+                    <p class="text-center fc-fff fs18 mt10">生产管理系统</p>
+                    <p class="text-center fc-fff f12">当前版本2.0</p>
+                </li>
+            </ul>
+        </div>
+        <div class="fc-fff pad0 f16" style="padding-top:18px;overflow:hidden;">
+            <span class="mr35">种植管理</span>
+            <span class="mr35">人员管理</span>
+            <span class="mr35">物料管理</span>
+            <span class="mr35">生产汇总表</span>
+            <span class="mr35">消息</span>
+            <span class="">基础设置</span>
+        </div>
+        <div class="pad0 w415 pull-right" style="position:absolute;top:0;right:10px;">
+            <p class="text-right fc-fff">
+                <i class="icon-angle-left f16 ml15"></i>
+                <i class="icon-angle-right f16 ml15"></i>
+                <i class="glyphicon glyphicon-repeat f14 ml15"></i>
+                <span class="f14 ml15">提交反馈</span>
+                <span class="f14 ml15">使用帮助</span>
+                <span class="f14 ml15">摩登庄园</span>
+                <i class="icon-minus f14 ml10"></i>
+                <i class="icon-check-empty f14 ml10"></i>
+                <i class=" icon-remove f14 ml10 mr10"></i>
+            </p>
+            <p class="text-right fc-fff">
+                <i class="fa fa-user-o mr10"></i>
+                <span class="mr10">姓名</span>
+                <span class="mr10">职务</span>
+                <i class="icon-caret-down mr10 mt5"></i>
+            </p>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+  name: 'top'
+}
+</script>
+
+<style scoped>
+
+</style>
